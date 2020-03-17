7123140c87c1902086e167ce6e7e1e8cd113f765
solly <214293939@mycput.ac.za>
2020-03-17 21:49:31 +0200

    Second commit with tests...

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -47,9 +47,25 @@ public class Person implements Comparable<Person>
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Person person = (Person) o;
 
+        if (age != person.age) return false;
+        if (!name.equals(person.name)) return false;
+        return title.equals(person.title);
+    }
 
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
 
     @Override
     public int compareTo(Person person) {
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,86 @@ package org.example;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAssertList() {
+
+        Person persons1 = new Person("Linda", "Minister", 52);
+        Person persons2 = new Person("Mike", "IT Specialist", 36);
+        Person persons3 = new Person("Jim", "Teacher", 42);
+        Person persons4 = new Person("Sizwe", "Student", 24);
+
+
+        List<Person> memberList = new ArrayList<>();
+        memberList.add(persons1);
+        memberList.add(persons2);
+        memberList.add(persons3);
+        memberList.add(persons4);
+
+        assertThat(memberList, hasItems(persons1, persons2, persons3, persons4));
     }
-}
+
+        @Test
+        public void testMap(){
+            Person persons1 = new Person("Linda", "Minister", 52);
+            Person persons2 = new Person("Mike", "IT Specialist", 36);
+            Person persons3 = new Person("Jim", "Teacher", 42);
+            Person persons4 = new Person("Sizwe", "Student", 24);
+
+            Map<String, String> map = new HashMap<>();
+            map.put("Linda","Minister");
+            map.put("Mike","IT Specialist");
+            map.put("Jim","Teacher");
+
+
+            Map<String, String> expected = new HashMap<>();
+            expected.put("Mike","IT Specialist");
+            expected.put("Jim","Teacher");
+            expected.put("Linda","Minister");
+
+
+            assertThat(map, is(expected));
+
+        }
+
+        @Test
+    public void testSort(){
+            Person persons1 = new Person("Linda", "Minister", 52);
+            Person persons2 = new Person("Mike", "IT Specialist", 36);
+            Person persons3 = new Person("Jim", "Teacher", 42);
+            Person persons4 = new Person("Sizwe", "Student", 24);
+
+
+            List<Person> memberList = new ArrayList<>();
+            memberList.add(persons1);
+            memberList.add(persons2);
+            memberList.add(persons3);
+            memberList.add(persons4);
+
+
+
+            //(memberList, contains("a", "b", "c"));
+        }
+
+
+
+
+
+
+
+
+    }
